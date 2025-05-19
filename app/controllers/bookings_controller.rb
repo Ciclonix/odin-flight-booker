bc4219bca7ad94bb5a03a3a4fa456f7eecc1e175
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2025-05-19 13:14:32 +0200

    Fix render when booking save fails

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,16 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
-    @num_tickets = params[:num_tickets].to_i
-    @flight = Flight.find(params[:flight_id])
+    @flight = flight_finder
+    @num_tickets = tickets_params
     @num_tickets.times { @booking.passengers.build }
   end
 
   def create
     @booking = Booking.new(booking_params)
+    @flight = flight_finder
+    @num_tickets = tickets_params
+
     if @booking.save
       flash[:notice] = "Flight booked!"
       redirect_to flights_path
@@ -20,5 +23,13 @@ class BookingsController < ApplicationController
   private
     def booking_params
       params.require(:booking).permit(:flight_id, :num_tickets, passengers_attributes: [ :name, :email ])
+    end
+
+    def flight_finder
+      Flight.find(params[:booking][:flight_id])
+    end
+
+    def tickets_params
+      params[:booking][:num_tickets].to_i
     end
 end
